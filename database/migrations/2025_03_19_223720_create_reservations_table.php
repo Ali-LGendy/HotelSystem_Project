d9757a281a4e73b175b2ec51a72cbef867f691f8
ahmedaljendy <elgendya166@gmail.com>
2025-03-20 01:10:08 +0200

    add migrations for reservations, payments, and notifications tables

---
+++ database/migrations/2025_03_19_223720_create_reservations_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users');
+            $table->foreignId('room_id')->constrained('rooms');
+            $table->integer('accompany_number');
+            $table->integer('price_paid');
+            $table->foreignId('receptionist_id')->nullable()->constrained('users');
+            $table->date('check_in_date');
+            $table->date('check_out_date');
+            $table->enum('status', ['pending', 'confirmed', 'checked_in', 'checked_out', 'cancelled'])->default('pending');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};

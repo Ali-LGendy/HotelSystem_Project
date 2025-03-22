c178f3923acc50ded030c209c545212b1316ddda
Ali-LGendy <ali.elgondy@gmail.com>
2025-03-22 14:41:36 +0200

    created 2 notification classes to mail the client

--- app/Models/User.php
+++ app/Models/User.php
@@ -66,11 +66,6 @@ class User extends Authenticatable
         return $this->hasMany(Floor::class);
     }
 
-    public function notifications(): HasMany
-    {
-        return $this->hasMany(Notification::class);
-    }
-
     public function reservations(): HasMany // for both client and receptionist // check it
     {
         return $this->hasMany(Reservation::class);

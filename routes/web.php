781f8c575718138c32f96ff19d2422cd47c51fb1
Mohammed Essam <mohammed.essam.10101@gmail.com>
2025-03-23 22:39:02 +0200

    Adding ClientController Routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -20,5 +20,49 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+
+/* Client Routes */
+
+use App\Http\Controllers\ClientController;
+
+Route::middleware(['auth'])->group(function () {
+    Route::get('/clients', [ClientController::class, 'index'])
+        ->middleware('permission:manage clients')
+        ->name('clients.index'); // List Clients
+
+    Route::get('/clients/approved', [ClientController::class, 'indexApproved'])
+        ->middleware('role:receptionist')
+        ->name('clients.showApproved'); // Show Receptionist's Approved Clients
+
+    Route::get('/clients/create', [ClientController::class, 'create'])
+        ->middleware('permission:manage clients')
+        ->name('clients.create'); // Show Client Registration Form
+
+    Route::post('/clients', [ClientController::class, 'store'])
+        ->middleware('permission:manage clients')
+        ->name('clients.store'); // Register Client
+
+    Route::get('/clients/{client}', [ClientController::class, 'show'])
+        ->middleware('permission:manage clients')
+        ->name('clients.show'); // Show Client Details
+
+    Route::get('/clients/{client}/edit', [ClientController::class, 'edit'])
+        ->middleware('permission:manage clients')
+        ->name('clients.edit'); // Show Edit Form
+
+    Route::put('/clients/{client}', [ClientController::class, 'update'])
+        ->middleware('permission:manage clients')
+        ->name('clients.update'); // Update Client
+
+    Route::delete('/clients/{client}', [ClientController::class, 'destroy'])
+        ->middleware('permission:manage clients')
+        ->name('clients.destroy'); // Delete Client
+
+    Route::patch('/clients/{client}/approve', [ClientController::class, 'approve'])
+        ->middleware('permission:approve clients')
+        ->name('clients.approve'); // Approve Client
+});
+
+
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';

9fc5b8db5622d7116be147552761bf88827f205d
ahmedaljendy <elgendya166@gmail.com>
2025-03-27 14:00:02 +0200

    feat: add Chart.js dependency; update dashboard data for admin and receptionists with additional user metrics

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -19,14 +19,27 @@ class DashboardController extends Controller
         $user = auth()->user();
 
         if ($user->hasRole('admin')) {
+            
+            //render to the welcom page for the admin 
             return Inertia::render('Admin/Dashboard', [
-                'total_users' => User::count(),
+                'total_managers' => User::role('manager')->count(),
+                'total_clients' => User::role('client')->count(),
+                'total_receptionists' => User::role('receptionist')->count(),
                 'total_reservations' => Reservation::count(),
                 'total_revenue' => Reservation::sum('price_paid'),
+                'baned_users'=> User::where('is_banned', true)->count(),
+                'approved_clients'=> User::where('is_approved', true)->count(),
+                'pending_clients'=> User::where('is_approved', false)
+        ->whereDoesntHave('roles', function ($query) {
+            $query->whereIn('name', ['manager', 'receptionist', 'admin']);
+        })
+        ->count(),
                 'menuLinks' => [
                 ['title' => 'Manage Managers', 'href' => route('admin.users.managers.store')],
                 ['title' => 'Manage Receptionists', 'href' => route('admin.users.receptionists.store')],
                 ['title' => 'Manage Clients', 'href' => route('receptionist.clients.index')],
+                ['title' => 'Manage Floors', 'href' => route('floors.index')],
+                ['title' => 'Manage Rooms', 'href' => route('rooms.index')],
 
             ]
             ]);
@@ -51,7 +64,7 @@ class DashboardController extends Controller
             return Inertia::render('Admin/Dashboard',[
                 'clients_to_approve' => User::role('client')->where('is_approved', false)->get(),
                 'approved_clients' => User::role('client')->where('is_approved', true)->get(),
-                'menuLinks' => $this->getreceptionistMenuLinks()
+                'menuLinks' => $this->getreceptionistMenuLinks(),
                 // 'total_reservations' => Reservation::where('is_approved', true)->where('manager_id', $user->id),
                 // ['title' => 'Manage Floors', 'href' => route('floors.index')],
                 // ['title' => 'Manage Rooms', 'href' => route('rooms.index')],
